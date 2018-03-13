2850a206818e959cff365ea4a20f3f98dabf7ed3
dianneaa <dianne.aalde@gmail.com>
2018-03-13 20:46:14 +0200

    Lisasin Advert klassi.

---
+++ TarkvaraSpringBoot/src/main/java/helitehnika/TarkvaraSpringBoot/Advert.java
@@ -0,0 +1,25 @@
+package helitehnika.TarkvaraSpringBoot;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Advert {
+
+@Id
+@GeneratedValue
+
+int id;
+String title;
+String description;
+double price;
+String email;
+String phone;
+String category;
+String subcategory;
+}
